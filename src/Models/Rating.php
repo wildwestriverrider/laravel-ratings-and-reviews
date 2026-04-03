8029f1e89bb8f94537dadee97da6b05a3d17a7e1
wildwestriverrider <5050325+wildwestriverrider@users.noreply.github.com>
2026-04-03 11:19:48 +0000

    Fix styling

--- src/Models/Rating.php
+++ src/Models/Rating.php
@@ -6,7 +6,6 @@ use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\MorphTo;
-use Illuminate\Database\Eloquent\SoftDeletes;
 use InvalidArgumentException;
 
 class Rating extends Model
